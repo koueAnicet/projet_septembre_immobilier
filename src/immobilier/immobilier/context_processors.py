851d7ba7bad8e08ee3c43066cab39977ef5d8e09
koueAnicet <anicetkoue@gmail.com>
2022-09-26 17:20:38 +0000

    ajout fonction context processors

---
+++ src/immobilier/immobilier/context_processors.py
@@ -0,0 +1,16 @@
+from web import models
+from service  import models
+from authentication  import models
+
+
+def get_site_infos(request):
+    return ''
+
+
+def get_site_infos(request):
+    return ''
+
+
+
+def get_site_infos(request):
+    return ''
